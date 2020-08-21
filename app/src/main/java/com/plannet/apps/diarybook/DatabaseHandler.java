4097aa7edafdade3df5274817f9045b965bbd149
Muhammed Shihad <shihad@infobreez>
2020-08-21 19:24:25 +0530

    test commit

--- app/src/main/java/com/plannet/apps/diarybook/DatabaseHandler.java
+++ app/src/main/java/com/plannet/apps/diarybook/DatabaseHandler.java
@@ -58,6 +58,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.disableWriteAheadLogging();
     }
 
+
     public static synchronized void reload() {
         if (instance != null) {
             try {
