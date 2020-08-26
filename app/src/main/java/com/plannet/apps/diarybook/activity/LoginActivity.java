a1017ccb8d0842667b910a872a0839879533f0ad
ShihadP <70016580+ShihadP@users.noreply.github.com>
2020-08-26 19:41:39 +0530

    login

--- app/src/main/java/com/plannet/apps/diarybook/activity/LoginActivity.java
+++ app/src/main/java/com/plannet/apps/diarybook/activity/LoginActivity.java
@@ -1,17 +1,41 @@
 package com.plannet.apps.diarybook.activity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.plannet.apps.diarybook.MainActivity;
+import com.plannet.apps.diarybook.Preference;
 import com.plannet.apps.diarybook.R;
 
 
 public class LoginActivity extends AppCompatActivity {
+    Button submit;
+    EditText user_name, pass_word;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView( R.layout.activity_login);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_login );
+
+        submit = (Button) findViewById( R.id.login );
+        user_name = (EditText) findViewById( R.id.username );
+        pass_word = (EditText) findViewById( R.id.password );
+         submit.setOnClickListener( new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 if (user_name.getText().toString().equals( Preference.getUserName())&&
+                         (pass_word.getText().toString().equals(Preference.getPassword()))){
+                     Intent intent = new Intent(LoginActivity.this, MainActivity.class );
+                     LoginActivity.this.startActivity(intent);
+                 }else {
+                     pass_word.setError("Invalid Password");
+                 }
+             }
+         } );
+
     }
 }
